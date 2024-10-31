6d8a1837147280e65ccd995419655d975bf0f9f4
MilanLabuss <milanlabus29@gmail.com>
2024-10-31 15:43:43 +0000

    Initial Commit

---
+++ LearningGithub/LearningGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGithubApp.swift
+//  LearningGithub
+//
+//  Created by Milan Labus on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningGithubTests/LearningGithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGithubTests.swift
+//  LearningGithubTests
+//
+//  Created by Milan Labus on 31/10/2024.
+//
+
+import Testing
+@testable import LearningGithub
+
+struct LearningGithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
