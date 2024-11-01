cfdd6ed31d51eb53fa6aaa82771f9413b7863397
MilanLabuss <milanlabus29@gmail.com>
2024-11-01 14:26:07 +0000

    FEATURE: Added a symbol and options for RatingsView

--- LearningGithub/ContentView.swift
+++ LearningGithub/ContentView.swift
@@ -10,11 +10,19 @@ import SwiftUI
 struct ContentView: View {
     
     @State var currentRating = 0
+    @State var heartRating = 0
+    @State var pinRating = 0
+    
     
     var body: some View {
         
         RatingsView(maxRating: 5, currentRating: $currentRating)
             .padding()
+        
+        RatingsView(maxRating: 3, currentRating: $heartRating, width: 50, symbol: "heart", color: .red)
+            .padding()
+        
+        RatingsView(maxRating: 5, currentRating: $pinRating, width: 40, symbol: "pin", color: .blue)
         
     }
     
